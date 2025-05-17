1dda2ce5b0e39a32aeb3714edb2f63047b60c9cd
M Risky Maulana <riskimaulana307373@gmail.com>
2025-05-17 12:12:18 +0800

    Add files via upload

---
+++ dbkoneksi.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan901;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author riski
+ * tgl :17-mei-2025
+ */
+public class dbkoneksi {
+    static String JDBC_DRIVER="com.mysql.cj.jdbc.Driver";
+    static String DB_URL="jdbc:mysql://localhost/test";
+    static String DB_USER="root";
+    static String DB_PASS="";
+    static Connection konek;
+    
+    public Connection koneksi (){
+        try{
+            Class.forName(JDBC_DRIVER);
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+        }catch(ClassNotFoundException | SQLException e){
+            System.out.println("Terjadi Masalah Koneksi DataBase");
+            
+        } 
+        return null;
+    }
+}
